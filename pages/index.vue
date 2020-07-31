cf0df5464def1b9ceab60a850e187c957efaa49e
koki <kkttm530@gmail.com>
2020-07-31 14:47:10 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div>This is Housekeeping Book System</div>
+  </div>
+  <!-- <div class="container">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+  </div>-->
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style>
+</style>
